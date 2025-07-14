fb3f984bd0de2799ed98722475abd3e259f1cf23
Mu He <mu.he@live.com>
2025-07-14 18:24:49 +0200

    Updates

---
+++ sw.js
@@ -0,0 +1,65 @@
+const CACHE_NAME = 'home-inventory-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/static/js/bundle.js',
+  '/static/css/main.css'
+];
+
+// 安装Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// 激活Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// 拦截网络请求
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // 如果缓存中有响应，返回缓存的响应
+        if (response) {
+          return response;
+        }
+        
+        // 否则从网络获取
+        return fetch(event.request)
+          .then((response) => {
+            // 检查响应是否有效
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            // 克隆响应
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          });
+      })
+  );
+}); 
